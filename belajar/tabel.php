78b0b87885b82f1ea86c2f2c26287ba9110bb36f
denniw <dennyclue@gmail.com>
2017-05-02 18:22:26 +0700

    include & require template

---
+++ belajar/tabel.php
@@ -0,0 +1,16 @@
+<table border="1" cellspacing="0" cellpadding="10">  
+	<thead>
+		<tr>
+			<th>NO</th>
+			<th>NIM</th>
+			<th>Nama</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>025</td>
+			<td>Falcon</td>
+		</tr>
+	</tbody>
+</table>
